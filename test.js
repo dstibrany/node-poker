38ce0251199c128f9a8f5109555f9e512f7c1dde
Dave Stibrany <daves@hubdoc.com>
2012-12-11 01:43:08 -0500

    hand analysis WIP

--- test.js
+++ test.js
@@ -1,11 +1,23 @@
+var util = require('util');
 var Dealer = require('./lib/dealer.js');
-var dealer = new Dealer();
+var HandAnalyzer = require('./lib/handAnalyzer.js');
 var numCards = process.argv[2] || 1;
 
-var hand = dealer.dealCards(numCards);
+var dealer = new Dealer();
+var board = dealer.dealCards(5);
+var handAnalyzer = new HandAnalyzer(board);
+var count = 0;
+do {
+    var hand1 = dealer.dealCards(2);
+    count++;
+    handAnalyzer.analyze(hand1);
+    console.log('Cards ', board.print(), hand1.print());
+}
+while (!handAnalyzer.isFlush());
 
-var hand = hand.map(function(card) {
-    return card.toString();
-});
+console.log(util.inspect(handAnalyzer));
+console.log('Took %s deals', count);
 
-console.log(hand);
+
+
+
