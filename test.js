e3980349d168d5cb162b1f97fecbcb7ec7260024
Dave Stibrany <davestibrany@gmail.com>
2012-12-14 01:39:50 -0500

    hand analyzer WIP

--- test.js
+++ test.js
@@ -5,18 +5,26 @@ var numCards = process.argv[2] || 1;
 
 var dealer = new Dealer();
 var board = dealer.dealCards(5);
-var handAnalyzer = new HandAnalyzer(board);
-var count = 0;
-do {
-    var hand1 = dealer.dealCards(2);
-    count++;
-    handAnalyzer.analyze(hand1);
-    console.log('Cards ', board.print(), hand1.print());
-}
-while (!handAnalyzer.isFlush());
 
-console.log(util.inspect(handAnalyzer));
-console.log('Took %s deals', count);
+console.log(board.sort().print());
+
+var spliced = board.splice('A');
+console.log("Spliced: ", spliced.print());
+console.log("board:", board.print());
+
+
+// var handAnalyzer = new HandAnalyzer(board);
+
+// do {
+//     var hand1 = dealer.dealCards(2);
+//     count++;
+//     handAnalyzer.analyze(hand1);
+//     console.log('Cards ', board.print(), hand1.print());
+// }
+// while (!handAnalyzer.isFlush());
+
+// console.log(util.inspect(handAnalyzer));
+// console.log('Took %s deals', count);
 
 
 
