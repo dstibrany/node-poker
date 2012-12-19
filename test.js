f54764219481fe3927162ce11b300c4aa8762059
Dave Stibrany <davestibrany@gmail.com>
2012-12-19 02:35:40 -0500

    hand analyzer detecting hands minus straight flush

--- test.js
+++ test.js
@@ -7,22 +7,28 @@ var numCards = process.argv[2] || 1;
 var dealer = new Dealer();
 var board = dealer.dealCards(5);
 var hand = dealer.dealCards(2);
-console.log(hand.print(), board.print())
-
+// var board = new Cards('6h5hTdQd4h')
+// var hand = new Cards('2h3h')
+console.log('Hand:', hand.print());
+console.log('Board:', board.print());
 
 var handAnalyzer = new HandAnalyzer(board);
+var finalHand = handAnalyzer.analyze(hand);
+console.log('\nResults:', finalHand.handType, finalHand.finalHand.print());
+// console.log(handAnalyzer.isFullHouse())
+// console.log(handAnalyzer.isTwoPair())
 
-var count = 0;
-var lowEnd;
-do {
-    var hand1 = dealer.dealCards(2);
-    count++;
-    handAnalyzer.analyze(hand1);
-    console.log('Cards ', board.print(), hand1.print());
-}
-while (!handAnalyzer.isStraight());
-console.log(handAnalyzer.allCards.spliceStraight(handAnalyzer.isStraight()).print());
-console.log('Took %s deals', count);
+// var count = 0;
+// var lowEnd;
+// do {
+//     var hand1 = dealer.dealCards(2);
+//     count++;
+//     handAnalyzer.analyze(hand1);
+//     console.log('Cards ', board.print(), hand1.print());
+// }
+// while (!handAnalyzer.isStraight());
+// console.log(handAnalyzer.allCards.spliceStraight(handAnalyzer.isStraight()).print());
+// console.log('Took %s deals', count);
 
 
 
