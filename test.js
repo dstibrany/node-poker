c106774030fc0af32cec98ccef4d5612052426bd
Dave Stibrany <daves@hubdoc.com>
2012-12-23 21:19:04 -0500

    fix bug in parsing straights

--- test.js
+++ test.js
@@ -7,20 +7,44 @@ var numCards = process.argv[2] || 1;
 var dealer = new Dealer();
 // var board = dealer.dealCards(5);
 // var hand = dealer.dealCards(2);
+// var hand2 = dealer.dealCards(2);
 
-var board = new Cards([]);
-// var hand = new Cards('AsAdAhTsTd')
-// var hand = new Cards('JdKsQsTs9s')
-// var hand = new Cards('3s3d3c3h9s')
-var hand = new Cards('3d2d7d9dTd')
-// console.log('Hand:', hand.print());
+board = new Cards([]);
+hand = new Cards('As2d3d4d5d');
+
+
+// console.log('Boo Hand:', hand.print());
+// console.log('Pokey Hand:', hand2.print());
 // console.log('Board:', board.print());
 
 var handAnalyzer = new HandAnalyzer(board);
 var finalHand = handAnalyzer.analyze(hand);
-console.log('\nResults:', finalHand.handType, finalHand.finalHand.print(), finalHand.weight);
-// console.log(handAnalyzer.isFullHouse())
-// console.log(handAnalyzer.isTwoPair())
+// var finalHand2 = handAnalyzer.analyze(hand2);
+console.log(finalHand.handType, finalHand.hand.print(), finalHand.weight);
+// console.log('Pokey Results:', finalHand2.handType, finalHand2.hand.print(), finalHand2.weight);
+
+// if (finalHand.weight > finalHand2.weight)
+//     console.log('\nBoo Wins!');
+// else if (finalHand.weight < finalHand2.weight)
+//     console.log('\nPokey Wins!');
+// else
+//     console.log('\nSplit pot');
+
+// inspect blow up
+/* Boo Hand: [ '4s', '9h' ]
+   Pokey Hand: [ '2d', 'As' ]
+   Board: [ 'Ah', '3c', '8d', '2s', '5h' ]
+*/
+
+
+
+
+
+
+
+
+
+
 
 // var count = 0;
 // var lowEnd;
