454b7c1ba492d0dbb9ae632af3ef334c10071225
Dave Stibrany <daves@hubdoc.com>
2014-09-22 20:30:39 -0400

    add some comments to simulate.js to better explain it

--- simulate.js
+++ simulate.js
@@ -3,22 +3,31 @@ var utils  = require('./lib/utils')
 var Player = require('./lib/player');
 var Table  = require('./lib/table');
 
+// create a table
 var table   = new Table('Table1', 6, 10);
+
+// create players with initial chip counts
 var player1 = new Player('Dave', 1000);
 var player2 = new Player('Pokey', 1000);
 var player3 = new Player('Willy', 1000);
 
+// join table
 player1.joinTable(table);
 player2.joinTable(table);
 player3.joinTable(table);
 
+// choose a seat an buy-in
 player1.sit('Table1', 0, 500);
 player2.sit('Table1', 1, 500);
 player3.sit('Table1', 2, 500);
 
+// sit in next hand
 player1.sitIn('Table1');
 player2.sitIn('Table1');
-// player3.sitIn('Table1');
+
+// once two players are sitting at a table, the game loop starts
+// so player3 won't actually be 'in' the hand until the next round
+player3.sitIn('Table1');
 
 process.stdin.setEncoding('utf8');
 
@@ -45,9 +54,6 @@ process.stdin.on('data', function (chunk) {
     }
     if (action) {
         process.stdin.pause();
-        currentPlayer.doAction(table.name, action); 
+        currentPlayer.doAction(table.name, action);
     }
 });
-
-
-
